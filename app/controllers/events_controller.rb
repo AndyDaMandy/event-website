a4c1bfdbdb3922ee65da64de1a1db94d67c12ce2
AndyDaMandy <maldonady.andy.m@gmail.com>
2022-08-14 14:00:17 -0400

    added event controllers

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,28 @@
 class EventsController < ApplicationController
   def index
+    @events = current_user.events.all
+  end
+
+  def show
+    @event = current_user.events.find(params[:id])
+  end
+
+  def new
+    @event = current_user.events.build
+  end
+  def edit
+    @event = current_user.events.find(params[:id])
+  end
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def event_params
+    params.require(:event).permit(:creator_id)
   end
 end
