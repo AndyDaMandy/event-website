4441219083cd092a3f948c9eca5702c2bec71d40
AndyDaMandy <maldonady.andy.m@gmail.com>
2022-08-14 15:16:54 -0400

    setting up index, need association fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
     before_action :authenticate_user!
+
+    def after_sign_in_path_for(resource)
+        user_events_path(current_user)
+     end
+     def after_sign_out_path_for(resource)
+         root_path
+     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   get 'events/index'
   root 'events#index'
+  resources :users do
+    resources :events
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
