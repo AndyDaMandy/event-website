e081cc279423a0de5851a2ac8b6f928b32a766d2
AndyDaMandy <maldonady.andy.m@gmail.com>
2022-08-21 10:49:53 -0400

    added through to event and user models

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,5 +2,6 @@ class Event < ApplicationRecord
     belongs_to :creator, class_name: "User"
     belongs_to :attendee, class_name: "User", optional: true
     has_many :event_attendees, foreign_key: :attendee_id
+    has_many :event_attendees, through: :event_attendings, source: :event_attendee
     validates :attendee, presence: true, allow_nil: true
 end
