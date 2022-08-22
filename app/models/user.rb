2ef5d024029a73a3a55767f843658ba1e7eb5e73
AndyDaMandy <maldonady.andy.m@gmail.com>
2022-08-22 15:42:06 -0400

    fixed associations, can now add attendees

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many :created_events, foreign_key: "creator_id", class_name: "Event"
-  has_many :event_attendings, foreign_key: "attendee_id"
-  has_many :attending_events, through: :event_attendings, source: :attendee
+  has_many :created_events, foreign_key: :creator_id, class_name: "Event"
+  has_many :event_attendings, foreign_key: :attendee_id
+  has_many :attending_events, through: :event_attendings, source: :event
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
