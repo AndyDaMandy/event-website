7031883a23c0af46c7957123d41fd7c06209ba76
AndyDaMandy <maldonady.andy.m@gmail.com>
2022-08-22 09:41:27 -0400

    removed extra

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :created_events, foreign_key: "creator_id", class_name: "Event"
   has_many :event_attendees, foreign_key: "attendee_id"
-  has_many :attended_events, through: :event_attendings, source: :attendee
+  has_many :attending_events, through: :event_attendings, source: :attendee
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
